610c44ed48768de09c2806ea91966fa1c9644982
Chris Occhipinti <cocchipinti@outbrain.com>
2016-01-01 18:39:15 -0500

    changed name to 'instalistr-controllers'

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 module.exports = {
-  controllers: {
-    list: require('./controllers/list'),
-    location: require('./controllers/location'),
-    post: require('./controllers/post'),
-    user: require('./controllers/user')
-  },
-  models: {
-    list: require('./models/list'),
-    location: require('./models/location'),
-    post: require('./models/post'),
-    user: require('./models/user')
-  }
+  list: require('./controllers/list'),
+  location: require('./controllers/location'),
+  post: require('./controllers/post'),
+  user: require('./controllers/user')
 };
